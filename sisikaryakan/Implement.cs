93e8b19ae6982a28ffd82cee89a583113ea4758d
nuragus <nuragus.linux@gmail.com>
2016-02-05 15:41:21 +0700

    Implementation of classes

---
+++ sisikaryakan/Implement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisikaryakan
+{
+    class Implement
+    {
+        public static void Main()
+        {
+            Manager tegar = new Manager("Manager Drilling");
+            Manager haidar = new Manager("Manager Drilling");
+            Manager sani = new Manager("Manager Refinery");
+            Manager adi = new Manager("Manager General Affairs");
+
+            Supervisor iqbal = new Supervisor("Supervisor Drilling Riau");
+            Supervisor ucu = new Supervisor("Supervisor Drilling Luwuk");
+            Supervisor deni = new Supervisor("Supervisor Refinery Balikpapan");
+
+            Staff dwi = new Staff("Staff Drilling Riau 1");
+            Staff trio = new Staff("Straff Drilling Riau 2");
+            Staff nuragus = new Staff("Staff Drilling Luwuk");
+            Staff sofi = new Staff("Staff Refinery Balikpapan");
+            Staff seta = new Staff("Staff General Affairs");
+
+            bool tegar_entry = tegar.Enter();
+            bool haidar_entry = haidar.Enter();
+            bool sani_entry = sani.Enter();
+            bool adi_entry = adi.Enter();
+
+            tegar.basicSalary = 10000000;
+            haidar.basicSalary = 10000000;
+
+            bool iqbal_entry = tegar.Enter();
+            //etc...etc....
+
+        }
+
+
+    }
+}
